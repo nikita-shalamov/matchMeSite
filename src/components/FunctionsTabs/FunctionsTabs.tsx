7e06f99cb6b75d13e2bec0497742e50dd2f2481c
nikita-shalamov <nikita.shalamovv@yandex.ru>
2024-10-21 11:55:18 +0500

    add preload video

--- src/components/FunctionsTabs/FunctionsTabs.tsx
+++ src/components/FunctionsTabs/FunctionsTabs.tsx
@@ -84,7 +84,7 @@ const FunctionsTabs = () => {
                 <div className="functions__items-block">
                     <div className="functions__video-wrapper">
                         {curValue && curValue.video && (
-                            <video key={curValue.video} autoPlay loop muted className="functions__video">
+                            <video key={curValue.video} autoPlay loop muted className="functions__video" preload="auto">
                                 <source src={curValue.video} type="video/mp4" />
                                 {t("errorVideo")}
                             </video>
